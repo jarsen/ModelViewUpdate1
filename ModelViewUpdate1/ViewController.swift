fc7fd7df7109e5cc054c509130bf6c006b69f93e
Jason Larsen <jason.larsen@gmail.com>
2016-05-14 09:02:48 -0600

    Initial Commit

---
+++ ModelViewUpdate1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModelViewUpdate1
+//
+//  Created by Jason Larsen on 5/14/16.
+//  Copyright © 2016 Jason Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
